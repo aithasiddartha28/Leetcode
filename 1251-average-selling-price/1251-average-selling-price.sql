7b0ef378fd96570edc4ee8b047794c395c874ef2
Aitha Siddartha <siddartha28105@gmail.com>
2026-09-04 14:57:05 +0530

    Time: 858 ms (65.2%), Space: 0B (100%) - LeetHub

---
+++ 1251-average-selling-price/1251-average-selling-price.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+select p.product_id,ifnull(round(sum(units*price)/sum(units),2),0) average_price 
+from prices p left join unitssold us
+on p.product_id=us.product_id and purchase_date between start_date and end_date
+group by p.product_id;
