9f43001e36e5add4226fe47cfe1a99db74d3dd34
Aitha Siddartha <siddartha28105@gmail.com>
2026-09-22 11:42:18 +0530

    Time: 0 ms (100%), Space: 19.2 MB (90.9%) - LeetHub

---
+++ 3516-find-closest-person/3516-find-closest-person.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findClosest(self, x: int, y: int, z: int) -> int:
+        diff_x=abs(z-x)
+        diff_y=abs(z-y)
+        if diff_x<diff_y:
+            return 1
+        elif diff_x>diff_y:
+            return 2
+        else:
+            return 0
